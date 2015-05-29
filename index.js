fdb9c062ff32a32a3a1059b5934bb76691d8629f
cluetrain <greenstrat67@gmail.com>
2015-05-28 19:00:53 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var hapi = require("hapi");
+var server = new hapi.Server();
+server.connection({ port: 8000});
+
+server.start(function() {
+  console.log("Server is started.")
+});
+
+server.views({
+  path: "views/templates",
+  layoutPath: "views",
+  layout: "default",
+  engines: {
+    html: require("handlebars")
+  },
+  isCached:false,
+  context: {
+    dev: true
+  }
+});
+
+server.route({
+  path: "/",
+  method: "GET",
+  handler: function(req, reply) {
+    reply.view("index", {title: "Simple Blog Site"});
+  }
+});
