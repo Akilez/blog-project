87e522fe5eb6a5e80861d8f563fafa43464f095a
cluetrain <greenstrat67@gmail.com>
2015-06-09 18:59:47 -0700

    All Posts method

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var hapi = require("hapi");
 var Post = require("./models/post.js");
+var Posts = require("./models/posts.js");
 var server = new hapi.Server();
 server.connection({ port: 8000});
 
@@ -48,15 +49,27 @@ server.route([
     }
   },
   {
-    path:"/posts/{title}",
+    path:"/posts/{title?}",
     method:"GET",
     handler: function(req, reply) {
       console.log(req.payload);
-      var post = new Post();
-      post.getPost(req.params.title, function(response) {
-        console.log("Got:", response);
-        reply.view("blog", response)
-      })
+      if (req.params.title) {
+        var post = new Post();
+        post.getPost(req.params.title, function(response) {
+          console.log("Got:", response);
+          if (response) {
+            reply.view("blog", {blog: response});
+          } else {
+            reply.view("blog", {err: "The requested post does not exist!!"});
+          }
+        });
+      } else {
+        var posts = new Posts();
+        posts.getAll(function(response) {
+          console.log("Got:", response);
+          reply.view("blog", {blog: response})
+        });
+      }
     }
   },
   {
--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,23 @@
 var Backbone = require("backbone");
+var Post = require("./post");
+var query = require("../db.js");
 
-module.exports = Backbone.Model.extend {
+var ALL = "SELECT * FROM posts ORDER BY date DESC"
 
-}
+module.exports = Backbone.Collection.extend({
+  model: Post,
+  getAll: function(complete) {
+    var self = this;
+    var stmt = query.db.prepare(ALL);
+    stmt.all(function(err, results){
+      self.add(results);
+      self.each(function(post) {
+        console.log(post.get("date"));
+        var jDate = Date(post.get("date"));
+        console.log(jDate);
+        post.set("date", jDate.toString());
+      });
+      complete(results);
+    });
+  }
+});
